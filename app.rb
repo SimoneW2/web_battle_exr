795d59306fc3425ee088ef512861142f418148ec
Simone <simonewlkr@hotmail.com>
2019-11-18 08:35:01 +0000

    Completed challenges Intro to the web up until the Capybara challenge.

--- app.rb
+++ app.rb
@@ -1,16 +1,32 @@
 require 'sinatra'
 class App < Sinatra::Application
 
-get '/' do
-  "Hello!"
-end
+# get '/' do
+#   "Hello!"
+# end
+#
+# get '/secret' do
+#   "This is a secret! shh!"
+# end
 
-get '/secret' do
-  "This is a secret! shh!"
-end
-
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)  
+  erb(:index)
+end
+
+get '/named-cat' do
+   params
+  @name = params[:name]
+  erb(:index)
+end
+
+get '/cat-form' do #form displaying route
+  erb:cat_form
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
 end
